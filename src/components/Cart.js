e789ec1fddb0055b106a93cd16eecc38365743e9
robert <matthewrueter51@gmail.com>
2022-09-15 13:28:43 -0500

    Cart component will call smaller components to display cart items/information.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,9 +4,41 @@ import '../css/cart.css';
 export default function Cart () {
 	return(
 		<div id="blackOutScreen">
+			<button className="closeBtn">close</button>
+			<div id="currentCartLabel">Showing: cart / wishlist</div>
 			<div className="cartContainer">
-				<p>My cart!.Coming Soon</p>
+				<div id="cartItemsBox">
+					<CartItem />
+					<CartItem />
+				</div>
+				<OrderSummary />
 			</div>
+		</div>
+	)
+}
+
+function CartItem (){
+	return(
+		<div className="cartitem">
+			<div className="imageTest"></div>
+			<div>Sweater</div>
+			<div>Item-price</div>
+			<div className="quantitybox">
+				<div>2</div>
+				<div>+</div>
+				<div>-</div>
+			</div>
+			<div>Total</div>
+		</div>
+	)
+}
+function OrderSummary (){
+	return(
+		<div id="orderSummaryBox">
+			<h3>Order Summary</h3>
+			<p># of items.</p>
+			<p>order-total</p>
+			<button>Checkout</button>
 		</div>
 	)
 }
