a601367ab5a10602decfec365ff4ac36f3d994b3
robert <matthewrueter51@gmail.com>
2022-09-16 15:45:19 -0500

    center layout of individual blog items.

--- src/footer_links/Blog_page.js
+++ src/footer_links/Blog_page.js
@@ -9,13 +9,38 @@ export default function BlogPage (){
 	return (
 		<div id='blogBackground'>
 			<Navbar />
-			<h1>Blog</h1>
-			<p>Some text</p>
-			<p>Some text</p>
-			<p>Some text</p>
-			<p>Some text</p>
-			<p>Some text</p>
+			<main className="lightMain" id="blogMain">
+				<h1>Blog</h1>
+				<p>Enjoy articles written by staff and community members. 
+				Topics range from fashion, to small business management, 
+				crafting to sustainable living.
+				</p>
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+				<BlogPost />
+			</main>
 			<Footer />
+		</div>
+	)
+}
+
+function BlogPost () {
+	return(
+		<div className="blogPost">
+			<div>Image</div>
+			<h4>Title of the Blog</h4>
+			<div className="blogSummary">In this blog someone will tell us all about 
+			....etc...etc...etc...etc... etc... etc...etc.. .etc... etc...
+			etc...etc...etc... etc...etc... etc...etc...etc... etc...etc...
+			 etc...etc...etc...
+			</div>
 		</div>
 	)
 }
