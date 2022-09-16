589aef18f15b8d255171ee45f61d1922a5250f8c
robert <matthewrueter51@gmail.com>
2022-09-16 14:47:45 -0500

    create PhotoCredits and Careers page skeletons/ added <main> styles in index.css.

--- src/footer_links/Careers_page.js
+++ src/footer_links/Careers_page.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
+import './css/careers_page.css';
+
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function CareersPage (){
 	return (
-		<div>
+		<div id="careersBackground">
 			<Navbar />
-			<h1>Work with us.</h1>
+			<div className="pageContainer">
+				<header className="pageHeader">
+					<h1>Come work with us.</h1>
+					<p className="lightText">Working for us etc...Working for us etc...
+					Working for us etc...Working for us etc...Working for us etc...Working for us etc...
+					Working for us etc...Working for us etc...Working for us etc...Working for us etc...
+					Working for us etc...Working for us etc...Working for us etc...Working for us etc...
+					Working for us etc...Working for us etc...
+					</p>
+					<div id="office"></div>
+				</header>
+				<main>
+					<h2 className="lightText">Available positions:</h2>
+					<p className="lightText">Even if what you're looking for isn't available, 
+					we'd always like to hear from you. 
+					</p>			
+				</main>
+
+			</div>
 			<Footer />
 		</div>
 	)
--- src/footer_links/PhotoCredits_page.js
+++ src/footer_links/PhotoCredits_page.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+import './css/photoCredits_page.css';
+
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function PhotoCreditsPage () {
 	return (
-		<div>
+		<div id="photoCreditsBackground">
 			<Navbar />
-			<h1>Thanks for the photos.</h1>
+			<div className="pageContainer">
+				<header className="pageHeader">
+					<div id="camera"></div>
+					<div className="pusherLarge"></div>
+					<h1>Thanks for the photos.</h1>
+				</header>
+				<main>
+				<p className="lightText">We'd like to thank the very talented photographers whose work is displayed throughout this site.</p>
+				<ul className="lightText">
+					<li>name here</li>
+					<li>name here</li>
+					<li>name here</li>
+					<li>name here</li>
+					<li>name here</li>
+					<li>name here</li>
+					<li>name here</li>
+					<li>name here</li>
+				</ul>
+				</main>
+			</div>
 			<Footer />
 		</div>
 	)
