0fd532e9f91870f38a67a878f40ff8a7f4bf0065
robert <matthewrueter51@gmail.com>
2022-09-15 20:19:45 -0500

    add backgroun image to WorkshopPage.

--- src/footer_links/Workshops_page.js
+++ src/footer_links/Workshops_page.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import '../css/index.css';
+
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function WorkshopsPage () {
+	
 	return (
-		<div>
+		<div id="workshopBackground">
 			<Navbar />
 			<h1>Workshops</h1>
+			<h1>some text</h1>
+			<h1>some text</h1>
 			<Footer />
 		</div>
 	)
