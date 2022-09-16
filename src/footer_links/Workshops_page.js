e5041433b2e99cc4a3c9705dfa71bcbf76729985
robert <matthewrueter51@gmail.com>
2022-09-16 10:32:31 -0500

    create css folder for Footer link pages.

--- src/footer_links/Workshops_page.js
+++ src/footer_links/Workshops_page.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import '../css/index.css';
-
+import '../images/woolset.jpg';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function WorkshopsPage () {
-	
+	const body = document.getElementsByTagName('body')[0];
+	body.style.backgroundImage="url(./woolset.jpg)";
 	return (
-		<div id="workshopBackground">
+		<div>
 			<Navbar />
 			<h1>Workshops</h1>
 			<h1>some text</h1>
