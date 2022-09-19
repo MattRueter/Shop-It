ca195a8926ddd65e69c22e87363c72f76454f076
robert <matthewrueter51@gmail.com>
2022-09-19 12:59:53 -0500

    create stock DB and import into Shop file.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
+
 import '../css/shop.css';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
 import Cart from './Cart.js';
+import stock from '../data/stock.js';
+
+//import sweater from '../images/sweater.jpg';
+
 
 export default function Shop (){
-	
-	
+	const [image, setImage] =useState(stock[0].img);
+	const [product, setProduct] =useState(stock[0].product);
+	const [price, setPrice] = useState(stock[0].price)
+
+	//handleClick here
+
 	return(
 		<div id="shopMain">
 			<Navbar />
@@ -14,7 +24,7 @@ export default function Shop (){
 			<div id="shoppingContainer">
 				<div id="displayCurrent">Showing: /All items</div>
 				<div id="cardContainer">
-					<Card />
+					<Card img={image} product={product} price={price} />
 					<Card />
 					<Card />
 					<Card />
@@ -32,13 +42,17 @@ export default function Shop (){
 	)
 }
 
-function Card (){
+function Card ({img,product, price}){
+
+
 	return (
 		<div className="card">
-			<div className="cardImage"></div>
-			<div>Item Title</div>
-			<div>$60</div>
+			<img  alt={product} className="cardImage"></img>
+			<div>{product}</div>
+			<div>{price} </div>
 			<button>details</button>
 		</div>
 	)
 }
+
+//<div style={{backgroundImage:`url(${img})`}}  className="cardImage"></div>
---
+++ src/data/stock.js
@@ -0,0 +1,7 @@
+
+
+const stock =[
+	{product:'sweater', price:47, img:"../images/sweater.jpg", details:"This sweater is made la la la la..."}
+]
+
+export default stock;
