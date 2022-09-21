1614dbf09c7311d59b3f7bdeae05cf8d53869669
robert <matthewrueter51@gmail.com>
2022-09-21 14:09:24 -0500

    add credits to credit page using const with photo credit information.

--- src/footer_links/PhotoCredits_page.js
+++ src/footer_links/PhotoCredits_page.js
@@ -5,7 +5,35 @@ import './css/photoCredits_page.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
+const credits =[
+	{img:"generic_clothing_rack",	credit:"" },
+	{img:"designer",				credit:"pexls.com/@cottonbro"},
+	{img:"chair_and_sweater", 		credit:"pexls.com/@karolina grabowska"},
+	{img:"dress", 					credit:"pexls.com/@the lazy artist gallery"},
+	{img:"jeans", 					credit:"pexls.com/@pixaby"},
+	{img:"sweater", 				credit:"pexls.com/@domj"},
+	{img:"camera", 					credit:"pexls.com/@atc comm photo"},
+	{img:"lamb", 					credit:"pexls.com/@kat smith"},
+	{img:"woolbackdrop", 			credit:"pexls.com/@engin-akyurt"},
+	{img:"woolset", 				credit:"pexls.com/@pavel danilyuk"},
+	{img:"office", 					credit:"pexls.com/@fauxels"},
+	{img:"v_neck_sweater", 			credit:"pexls.com/@ron-lach"},
+	{img:"dark_jeans", 				credit:"pexls.com/@pixaby"},
+	{img:"wool_socks", 				credit:"pexls.com/@lumn"},
+	{img:"handbag", 				credit:"pexls.com/@mart-production"},
+	{img:"cotton_shorts", 			credit:"pexls.com/@kristina-paukshtite"}
+];
+
+
 export default function PhotoCreditsPage () {
+	const photoCredits =credits.map((item,index)=>{
+		if(item.credit!=""){
+			return(
+				<li>{item.credit}</li>
+				)
+		}
+	});
+
 	return (
 		<div id="photoCreditsBackground">
 			<Navbar />
@@ -18,14 +46,7 @@ export default function PhotoCreditsPage () {
 				<main>
 				<p className="lightText">We'd like to thank the very talented photographers whose work is displayed throughout this site.</p>
 				<ul className="lightText">
-					<li>name here</li>
-					<li>name here</li>
-					<li>name here</li>
-					<li>name here</li>
-					<li>name here</li>
-					<li>name here</li>
-					<li>name here</li>
-					<li>name here</li>
+					{photoCredits}
 				</ul>
 				</main>
 			</div>
