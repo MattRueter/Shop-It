1879da6861228c8ba3089c540582062f165d5bda
robert <matthewrueter51@gmail.com>
2022-09-21 14:17:41 -0500

    create user cart variable to hold current cart items.

--- src/data/stock.js
+++ src/data/stock.js
@@ -8,15 +8,15 @@ import handBag from '../images/product_images/handbag.jpg';
 import cottonShorts from '../images/product_images/cotton-shorts.jpg';
 
 const stock =[
-	{product:'Sweater', 		price:47, img:sweater, 				   details:"Product details", 	id:"1"},
-	{product:'V neck sweater', 	price:57, img:vNeckSweater,			   details:"Some details..", 	id:"2"},
-	{product:'Jeans', 			price:62, img:darkJeans,			   details:"jeans details..", 	id:"3"},
-	{product:'Wool socks',	 	price:17, img:"../images/sweater.jpg", details:"Some details..", 	id:"4"},
-	{product:'Hand-bag',		price:37, img:"../images/sweater.jpg", details:"Some details..", 	id:"5"},
-	{product:'Shorts',		 	price:24, img:"../images/sweater.jpg", details:"Some details..", 	id:"6"},
-	{product:'Tie', 			price:32, img:"../images/sweater.jpg", details:"Some details..", 	id:"7"},
-	{product:'Pyjamas', 		price:28, img:"../images/sweater.jpg", details:"Some details..", 	id:"8"},
-	{product:'T-shirt', 		price:22, img:"../images/sweater.jpg", details:"Some details..", 	id:"9"},
+	{product:'Sweater', 		price:47, img:sweater, 				   details:"Product details",	quantity:10, 	id:"1"},
+	{product:'V neck sweater', 	price:57, img:vNeckSweater,			   details:"Some details..", 	quantity:10,	id:"2"},
+	{product:'Jeans', 			price:62, img:darkJeans,			   details:"jeans details..", 	quantity:10,	id:"3"},
+	{product:'Wool socks',	 	price:17, img:"../images/sweater.jpg", details:"Some details..", 	quantity:10,	id:"4"},
+	{product:'Hand-bag',		price:37, img:"../images/sweater.jpg", details:"Some details..", 	quantity:10,	id:"5"},
+	{product:'Shorts',		 	price:24, img:"../images/sweater.jpg", details:"Some details..", 	quantity:10,	id:"6"},
+	{product:'Tie', 			price:32, img:"../images/sweater.jpg", details:"Some details..", 	quantity:10,	id:"7"},
+	{product:'Pyjamas', 		price:28, img:"../images/sweater.jpg", details:"Some details..", 	quantity:10,	id:"8"},
+	{product:'T-shirt', 		price:22, img:"../images/sweater.jpg", details:"Some details..", 	quantity:10,	id:"9"},
 
 
 ]
---
+++ src/data/usrCart.js
@@ -0,0 +1,12 @@
+import sweater from '../images/product_images/sweater.jpg';
+import vNeckSweater from '../images/product_images/v-neck-sweater.jpg'
+import darkJeans from '../images/product_images/dark-jeans.jpg';
+
+
+const usrCart =[
+	{product:'Sweater', 		price:47, img:sweater, 		details:"Product details",	quantity:1,	id:"1"},
+	{product:'V neck sweater', 	price:57, img:vNeckSweater,	details:"Some details..",	quantity:1,	id:"2"},
+	{product:'Jeans', 			price:62, img:darkJeans,	details:"jeans details..",	quantity:1,	id:"3"}
+]
+
+export default usrCart;
