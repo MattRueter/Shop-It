4d9cc016a9d6eef60e860362509bbdb57badae7a
robert <matthewrueter51@gmail.com>
2022-09-23 16:34:47 -0500

    create a no-image photo to show if product has no image.

--- src/data/stock.js
+++ src/data/stock.js
@@ -1,6 +1,8 @@
 import sweater from '../images/product_images/sweater.jpg';
-import vNeckSweater from '../images/product_images/v-neck-sweater.jpg'
+import noImage from '../images/noImage.jpg';
+import vNeckSweater from '../images/product_images/v-neck-sweater.jpg';
 import darkJeans from '../images/product_images/dark-jeans.jpg';
+
 
 //to edit
 import woolSocks from '../images/product_images/wool-socks.jpg';
@@ -11,12 +13,12 @@ const stock =[
 	{product:'Sweater', 		price:47, img:sweater, 		details:"Product details",	quantity:10, 	id:"1"},
 	{product:'V neck sweater', 	price:57, img:vNeckSweater,	details:"Some details..", 	quantity:10,	id:"2"},
 	{product:'Jeans', 			price:62, img:darkJeans,	details:"jeans details..", 	quantity:10,	id:"3"},
-	{product:'Wool socks',	 	price:17, img:"none", 		details:"Some details..", 	quantity:10,	id:"4"},
-	{product:'Hand-bag',		price:37, img:"none", 		details:"Some details..", 	quantity:10,	id:"5"},
-	{product:'Shorts',		 	price:24, img:"none", 		details:"Some details..", 	quantity:10,	id:"6"},
-	{product:'Tie', 			price:32, img:"none", 		details:"Some details..", 	quantity:10,	id:"7"},
-	{product:'Pyjamas', 		price:28, img:"none", 		details:"Some details..", 	quantity:10,	id:"8"},
-	{product:'T-shirt', 		price:22, img:"none", 		details:"Some details..", 	quantity:10,	id:"9"},
+	{product:'Wool socks',	 	price:17, img:noImage, 		details:"Some details..", 	quantity:10,	id:"4"},
+	{product:'Hand-bag',		price:37, img:noImage, 		details:"Some details..", 	quantity:10,	id:"5"},
+	{product:'Shorts',		 	price:24, img:noImage, 		details:"Some details..", 	quantity:10,	id:"6"},
+	{product:'Tie', 			price:32, img:noImage, 		details:"Some details..", 	quantity:10,	id:"7"},
+	{product:'Pyjamas', 		price:28, img:noImage, 		details:"Some details..", 	quantity:10,	id:"8"},
+	{product:'T-shirt', 		price:22, img:noImage, 		details:"Some details..", 	quantity:10,	id:"9"},
 ]
 
 
