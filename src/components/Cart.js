6524c8daf3f9ac0c406f5cfa7116d6f728881efa
robert <matthewrueter51@gmail.com>
2022-09-23 15:54:08 -0500

    Cart layout update: restyling overhaul.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,28 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import '../css/cart.css';
 import usrCart from '../data/usrCart.js';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
 
 
-export default function Cart () {
-	const [currentCart, setCurrentCart]=useState(usrCart);
+export default function Cart() {
+	const [currentCart, setCurrentCart] = useState(usrCart);
 
 
-	const displayCart = currentCart.map((item)=>{
-		return(
+	const displayCart = currentCart.map((item, index) => {
+		return (
 			<CartItem
-			img={item.img} 
-			product={item.product} 
-			price={item.price} 
-			quantity={item.quantity} 
-			total={item.quantity * item.price} 
-			key={item.id} />
+				img={item.img}
+				product={item.product}
+				price={item.price}
+				quantity={item.quantity}
+				total={item.quantity * item.price}
+			/>
 		)
 	})
 
-	return(
-		<div id="cart">		
+	return (
+		<div id="cart">
 			<Navbar />
 			<div className="cartContainer">
 				<div id="cartItemsBox">
@@ -35,32 +35,41 @@ export default function Cart () {
 	)
 }
 
-function CartItem ({img,product,price,quantity,total}){
-	return(
+function CartItem({ img, product, price, quantity, total }) {
+	let currencies = { dollars: "$", euros: "€", pounds: "£" };
+	let crntCurrency = currencies.euros;
+
+	return (
 		<div className="cartitem">
 			<img src={img} alt={product} className="cartItemImage"></img>
-		
+
 			<div className="cartItemInfo">
 				<div>{product}</div>
-				<div>{price}</div>
+				<div>
+					<div>Item Price:</div>
+					<div>{crntCurrency}{price}</div>
+				</div>
 				<div className="quantitybox">
 					<div>{quantity}</div>
 					<div>+</div>
 					<div>-</div>
 				</div>
-				<div>{total}</div>
+				<div>
+					<div>Total:</div>
+					<div> {crntCurrency}{total}</div>
+				</div>
 			</div>
 
 		</div>
 	)
 }
-function OrderSummary (){
-	return(
+function OrderSummary() {
+	return (
 		<div id="orderSummaryBox">
 			<h3>Order Summary</h3>
 			<p>Items: 5</p>
 			<p>Total: $75.89</p>
-			<button className="cartBtn">Checkout</button>
+			<button id="checkoutBtn">Checkout</button>
 		</div>
 	)
 }
