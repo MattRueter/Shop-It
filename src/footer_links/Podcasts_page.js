6307ff7797ca32905d806127f66c10560bd91204
robert <matthewrueter51@gmail.com>
2022-09-23 12:51:15 -0500

    create header and main on Podcastspage.

--- src/footer_links/Podcasts_page.js
+++ src/footer_links/Podcasts_page.js
@@ -1,14 +1,43 @@
 import React from 'react';
 
+import './css/podcasts_page.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import {InfoCard} from './Blog_page.js';
 
-
+let podcastsDB =[
+	{
+	img:'an image', 
+	title:'Podcast Title', 
+	post:`In this episdode we talk
+	 about how you might go about 
+	 breeding your own sheep to produce your own wool.`
+	}
+]
 export default function PodcastsPage () {
+	const podcasts =podcastsDB.map((item,index)=>{
+		return(
+			<InfoCard image={item.img} title={item.title} post={item.post} />
+		)
+	})
+
 	return (
 		<div>
 			<Navbar />
-			<h1>Podcasts</h1>
+			<header className="lightText" >
+				<h1>Podcasts</h1>
+				<div id="headerContainer">
+					<h2>We have our own podcasts!</h2>
+					<p>
+					Join Edu and Maree as they chat about things related to 
+					making and selling clothes etc. etc..
+					</p>
+					<div id='podcastsPageImg'></div>
+				</div>
+			</header>
+			<main className="lightMain">
+				{podcasts}
+			</main>
 			<Footer />
 		</div>
 	)
